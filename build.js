a03d297867db0daec8a04699073c9c9b726b2a25
Ash Heskes <me@ashmokhberi.com>
2015-01-23 15:50:53 +0000

    Fixed build step ready for first release.

--- build.js
+++ build.js
@@ -2,11 +2,15 @@
 
 var FileSystem	= require ( 'fs' ),
 	jsProcessor	= require ( 'uglify-js' ),
-	js_file		= FileSystem. readFileSync ( './supportive.js' ),
+	js_file		= FileSystem. readFileSync ( './supportive.js' ). toString(),
 	ast			= jsProcessor. parser. parse ( js_file );
+
+console. log ( jsProcessor );
 
 ast		= jsProcessor. uglify. ast_mangle ( ast );
 ast		= jsProcessor. uglify. ast_squeeze ( ast );
 js_file	= jsProcessor. uglify. gen_code ( ast );
 
-FileSystem. outputFileSync ( './supportive.min.js', js_file );
+FileSystem. writeFileSync ( './supportive.min.js', js_file );
+
+console. log ( 'Minification Complete.' )
